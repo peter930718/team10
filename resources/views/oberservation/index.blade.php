e143fe16580df956bdddfd786f5d4837db4493e2
peter930718 <peter93342004@gmail.com>
2024-11-29 13:52:32 +0800

    update show and index

---
+++ resources/views/oberservation/index.blade.php
@@ -0,0 +1,32 @@
+@extends('app')
+
+@section('title', '臺灣鯨豚族群調查計畫-生態調查標準資料')
+
+@section('sdg_theme', '臺灣鯨豚族群調查計畫-生態調查標準資料')
+
+@section('sdg_contents')
+    <table border="1">
+            <tr>
+                <th>計畫/案件名稱</th>
+                <th>申請開始日</th>
+                <th>申請截止日</th>
+                <th>計畫名稱</th>
+                <th>國家</th>
+                <th>協議機構</th>
+            </tr>
+        @foreach ($oberservations as  $oberservation)
+            <tr>
+            <td >{{$oberservation->recruitment_year}}</td>
+            <td>{{$oberservation->application_start_date}}</td>
+            <td>{{$oberservation->application_deadline}}</td>
+            <td>{{$oberservation->project_name}}</td>
+            <td>{{$oberservation->country}}</td>
+            <td>{{$oberservation->agreement_agency}}</td>
+            </tr>
+                <td><a href="{{ route('oberservation.show', ['id' => $oberservation->id]) }}">顯示</a></td>
+                <td><a href="{{ route('oberservation.edit', ['id' => $oberservation->id]) }}">編輯</a></td>
+            </tr>
+        @endforeach
+    </table>
+
+@endsection
--- resources/views/oberservation/show.blade.php
+++ resources/views/oberservation/show.blade.php
@@ -2,17 +2,18 @@
 
 @section('title', '顯示表單')
 
-@section('nba_theme', '您所選取的球隊資料')
+@section('sdgs_theme', '您所選取的資料')
 
-@section('nba_contents')
-球隊編號：{{ $team->id }}<br/>
-球隊名字：{{ $team->name }}<br/>
-球隊所在城市：{{ $team->city }}<br/>
-球隊分區：{{ $team->zone }}<br/>
-球隊主場：{{ $team->home }}<br/>
+@section('sdgs_contents')
+徵求年度：{{ $team->recruitment_year }}<br/>
+申請開始日：{{ $team->application_start_date }}<br/>
+申請截止日：{{ $team->application_deadline }}<br/>
+計畫名稱：{{ $team->project_name }}<br/>
+國家：{{ $team->country }}<br/>
+協議機構：{{ $team->agreement_agency }}<br/>
 
 <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
-    {{ $team->name }}所有球員
+    {{ $team->name }}所有資料
 </div>
 <table>
     <tr>
