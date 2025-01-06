3dba78c38ff6d2de7bcf17c877213baf1759bead
GA502IU\USER <peter93342004@gmail.com>
2025-01-06 14:53:42 +0800

    update

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,6 +1,4 @@
-<div class="clear: both;text-align: center;">
-    <div class="footer;">
-        歡迎來到D1114182061的網站
-    </div>
-</div>
 
+<footer>
+    &copy; 2025由D1114182061。版權所有。
+</footer>
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,133 +1,37 @@
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>國際合作網頁</title>
-
-    <header>
-        <a href="https://github.com" target="_blank">
-            <img src="github.png" alt="GitHub" style="width: 70px; height: 70px;float:right;margin:10px;">
-        </a>
-        <h1 >國際合作網頁</h1>
-
-    </header>
-    <style>
-        header{
-            text-shadow: 3px 3px 1px black;
-            line-height: 1.6;
-            margin:0px;
-            text-align: left;
-            color: #fff;
-            background: #003049;
-            font-size: 30px;
-            border-radius: 0px 15px 15px 0px;
-        }
-    </style>
-    <style>
-        .sideMenu {
-            width: 300px;
-            height: 100%;
-            background-color: #003049;
-            border-right: 3px solid #d1d1d1;
-            display: flex;
-            flex-direction: column;
-            padding: 50px 0;
-            box-shadow: 5px 0 5px rgba(23, 23, 54, 0.6);
-            border-radius: 20px 0px 0px 20px;
-            position: relative;
-            transform: translateX(-100%);
-            transition: 0.5s;}
-            form {
-                display: flex;
-                margin: 0 10px 50px;
-                border-radius: 100px;
-                border: 1px solid #fff;
-
-                input {
-                  width: 85%;
-                }
-
-                button {
-                  width: 15%;
-                }
-
-                input,
-                button {
-                  border: none;
-                  padding: 5px 10px;
-                  background-color: transparent;
-                  color: #fff;
-                }
-                input:focus,
-                button:focus {
-                  outline: none;
-                }
-              }
-              nav {
-                a {
-                  display: block;
-                  color: #fff;
-                  padding: 20px 10px;
-                  position: relative;
-                  font-weight: 300;
-
-                  .fas {
-                    margin-right: 0em;
-                    transform: scale(0);
-                    transition: 0.3s;
-                  }
-
-                  &:hover .fas {
-                    margin-right: 1em;
-                    transform: scale(1);
-                  }
-
-                  & + a::before {
-                    content: "";
-                    position: absolute;
-                    border-top: 1px solid #ffffff;
-                    left: 10px;
-                    right: 10px;
-                    top: 0px;
-                  }
-                }
-              }
-              label {
-                width: 20px;
-                height: 80px;
-                background-color: #d1d1d1;
-                color: #686666;
-                position: absolute;
-                right: -20px;
-                top: 0;
-                margin: auto;
-                line-height: 80px;
-                text-align: center;
-                border-radius: 0 5px 5px 0;
-                box-shadow: 5px 0 5px rgba(23, 23, 54, 0.6);
-              }
-              #sideMenu-active:checked + .sideMenu {
-                transform: translateX(0);
-                label .fas {
-                  transform: scaleX(-1);
-                }
-              }
-              #sideMenu-active {
-                position: absolute;
-                opacity: 0;
-                z-index: -1;}
-    </style>
-</head>
-<body>
-    <input type="checkbox" name="" id="sideMenu-active" />
-
-    <div class="sideMenu">
-      <nav>
-        <a href="#"><i class="fas fa-backgroud"></i>計畫背景</a>
-        <a href="#"><i class="fas fa-list"></i>合作表單</a>
-        <a href="about-us.html"><i class="fas fa-about-us"></i>關於我們</a>
-      </nav>
-      <label for="sideMenu-active">
-        <i class="fas fa-angle-right"></i>
-      </label>
+<header>
+    <h1>網頁首頁</h1>
+    <div class="nav-buttons">
+        <div class="dropdown">
+            <button>Menu 1</button>
+            <div class="dropdown-content">
+                <a href="#">Option 1.1</a>
+                <a href="#">Option 1.2</a>
+                <a href="#">Option 1.3</a>
+            </div>
+        </div>
+        <div class="dropdown">
+            <button>Menu 2</button>
+            <div class="dropdown-content">
+                <a href="#">Option 2.1</a>
+                <a href="#">Option 2.2</a>
+                <a href="#">Option 2.3</a>
+            </div>
+        </div>
+        <div class="dropdown">
+            <button>合作表單</button>
+            <div class="dropdown-content">
+                <a href="#">Option 3.1</a>
+                <a href="#">Option 3.2</a>
+                <a href="#">Option 3.3</a>
+            </div>
+        </div>
+        <div class="dropdown">
+            <button>關於我們</button>
+            <div class="dropdown-content">
+                <a href="#">D1114182061</a>
+                <a href="#">Option 4.2</a>
+                <a href="#">小小心得</a>
+            </div>
+        </div>
     </div>
-      </body>
+</header>
